e8caa17de12420ae8a6d7a5116306076a86ac9bc
Nguyen Van ninh <79904488+ninh2771999@users.noreply.github.com>
2021-04-26 22:44:54 +0700

    Update ContestData.cs

--- DataLTK/ContestData.cs
+++ DataLTK/ContestData.cs
@@ -8,19 +8,19 @@ namespace DataLTK
 {
 	public class ContestData
 	{
-		private TracNghiemEntities tn;
+		private Multi_Choice_Entities tn;
 		public ContestData()
 		{
-			tn = new TracNghiemEntities();
+			tn = Multi_Choice_Entities();
 		}
 		public List<ch_db> ListQuiz()
 		{
 			var model = tn.ch_db.ToList();
 			return model;
 		}
-		public bool CheckQuiz(int maCauHoi, string dapAn)
+		public bool CheckQuiz(int id_ques, string answer)
 		{
-			var model = tn.Quizs.Count(x => x.MaCauHoi == maCauHoi && x.DapAn == dapAn);
+			var model = tn.Quizs.Count(x => x.id_ques == id_ques && x.answer == answer);
 			if(model > 0)
 			{
 				return true;
@@ -30,9 +30,9 @@ namespace DataLTK
 				return false;
 			}
 		}
-		public List<KetQua> ListKetQuaID(int maThiSinh, int maDe)
+		public List<Result> ListResultID(int id_examinee, int id_code)
 		{
-			var tan = tn.KetQuas.Where(x => x.MaDe == maDe && x.MaThiSinh == maThiSinh).ToList();
+			var tan = tn.Results.Where(x => x.code == code && x.id_examinee == examinee).ToList();
 			return tan;
 		}
 	}
